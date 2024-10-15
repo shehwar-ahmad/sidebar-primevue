8992277f4a33f84c8e5a9ba679287e29691c5872
shehwar-dev <shehwar.dev@gmail.com>
2024-10-16 01:10:28 +0500

    feat: basic project structure

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Drawer from 'primevue/drawer'
+import Button from 'primevue/button'
+
+import { ref } from 'vue'
+
+const visible = ref(false)
+</script>
+
+<template>
+  <div class="">
+    <Drawer v-model:visible="visible" header="Drawer" class="bg-white  p-1 py-20">
+      <p class="text-center text-red-500">Lorlat.</p>
+    </Drawer>
+    <div>
+      <Button @click="visible = true" class="bg-green-500 p-4 rounded m-4"> Show Sidebar </Button>
+    </div>
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,21 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './index.css'
+import PrimeVue from 'primevue/config'
 
 const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    options: {
+      cssLayer: {
+        name: 'primevue',
+        order: 'tailwind-base, primevue, tailwind-utilities',
+      },
+    },
+  },
+})
 
 app.use(router)
 
